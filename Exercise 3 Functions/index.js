3b1084a648b3c73319c0850f8aef9daa2d1f33d5
Clentan <105500834+Clentan@users.noreply.github.com>
2024-06-25 22:56:44 +0200

    3

---
+++ Exercise 3 Functions/index.js
@@ -0,0 +1,6 @@
+//Write a function that takes two numbers as parameters and returns their sum
+function addNumbers(a, b) {
+    return a + b;
+}
+var result = addNumbers(5, 10);
+console.log(result);
---
+++ Exercise 3 Functions/index.ts
@@ -0,0 +1,6 @@
+//Write a function that takes two numbers as parameters and returns their sum
+function addNumbers(a: number, b: number): number {
+    return a + b;
+  }
+  let result: number = addNumbers(5, 10);
+  console.log(result); 
