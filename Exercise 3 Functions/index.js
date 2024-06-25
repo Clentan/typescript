69a970946701092c3512bf2a5577522d0fc6cd5f
Clentan <105500834+Clentan@users.noreply.github.com>
2024-06-25 22:59:25 +0200

    2

--- Exercise 3 Functions/index.js
+++ Exercise 3 Functions/index.js
@@ -4,3 +4,9 @@ function addNumbers(a, b) {
 }
 var result = addNumbers(5, 10);
 console.log(result);
+//Write a function that returns a fixed number
+function getFixedNumber() {
+    return 20;
+}
+var result = getFixedNumber();
+console.log(result);
--- Exercise 3 Functions/index.ts
+++ Exercise 3 Functions/index.ts
@@ -4,3 +4,9 @@ function addNumbers(a: number, b: number): number {
   }
   let result: number = addNumbers(5, 10);
   console.log(result); 
+  //Write a function that returns a fixed number
+  function getFixedNumber(): number {
+    return 20;
+  }
+  let result = getFixedNumber();
+console.log(result); 
